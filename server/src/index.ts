953c6b20df33d2dcb4e1045dee642c532a6847aa
Laurent-JSON <lawrencetibasima@gmail.com>
2026-03-08 21:34:43 +0100

    Server modification

--- server/src/index.ts
+++ server/src/index.ts
@@ -38,9 +38,6 @@ app.use("/teams", teamRoutes);
 
 /* SERVER */
 const port = Number(process.env.PORT) || 8080;
-// app.listen(port, "0.0.0.0", () => {
-//   console.log(`Server running on part ${port}`);
-// });
-app.listen(port, () => {
-  console.log(`Server running on port... ${port}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on part ${port}`);
 });
